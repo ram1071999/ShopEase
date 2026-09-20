3a6db3acd59469abca9f657c2208d0fcb8e5a447
RAM VASHISHTHA <92358898+ram1071999@users.noreply.github.com>
2026-09-20 06:29:19 +0530

    Update OrderRepository.java

--- src/main/java/com/ecommerce/app/repository/OrderRepository.java
+++ src/main/java/com/ecommerce/app/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import com.ecommerce.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUserOrderByCreatedAtDesc(User user);
